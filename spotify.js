e6091bf6533bfd859c48b07acafd159633075b4f
unknown <jtownsend@5D2SFV1.mi.corp.rockfin.com>
2014-07-31 11:17:06 -0400

    Just formatting, I think

--- spotify.js
+++ spotify.js
@@ -1,36 +1,39 @@
-function getSpotifyTabs(fn) {
-	chrome.tabs.query({title: "* - Spotify"}, 
-		function(tabs) {
-			console.log(tabs);
-			$(tabs).each(function(t) {
-			fn(tabs[t]);
-		});
-	});
-}
-
 $(document).ready(function(e) {
 
+	function getSpotifyTabs(fn) {
+		chrome.tabs.query({title: "* - Spotify"}, 
+			function(tabs) {
+				console.log(tabs);
+				$(tabs).each(function(t) {
+				fn(tabs[t]);
+			});
+		});
+	}
+	
 	$('#prev').click(function(e) {
 		getSpotifyTabs(
-		function (t) {
-			console.log(t);
-			chrome.tabs.executeScript(t.id, { file:"./commands/fwd.js" });
-		});
+			function (t) {
+				console.log(t);
+				chrome.tabs.executeScript(t.id, { file:"./commands/fwd.js" });
+			}
+		);
 	});
 
 	$('#pause').click(function(e) {
 		getSpotifyTabs(
-		function (t) {
-			console.log(t);
-			chrome.tabs.executeScript(t.id, { file:"./commands/toggle_pause.js" });
-		});
+			function (t) {
+				console.log(t);
+				chrome.tabs.executeScript(t.id, { file:"./commands/toggle_pause.js" });
+			}
+		);
 	});
 
 	$('#fwd').click(function(e) {
 		getSpotifyTabs(
-		function (t) {
-			console.log(t);
-			chrome.tabs.executeScript(t.id, { file:"./commands/prev.js" });
-		});
+			function (t) {
+				console.log(t);
+				chrome.tabs.executeScript(t.id, { file:"./commands/prev.js" });
+			}
+		);
 	});
 });
